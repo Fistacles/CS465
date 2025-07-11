0b54ff458522780f2bf299d5f56134afcb0a8426
Fistacles <76632407+Fistacles@users.noreply.github.com>
2025-07-10 22:35:56 -0400

    Add files via upload

---
+++ app_server/routes/travel.js
@@ -0,0 +1,9 @@
+/* GET travel view */
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/travel');
+
+/*GET travel page. */
+router.get('/', controller.travel);
+
+module.exports = router;
